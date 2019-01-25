b7d2d963643a4075018b320b16cb1b73a1cb26bd
Липунов Денис Игоревич <Denis.Lipunov@tele2.ru>
2019-01-25 12:15:26 +0800

    Updates

---
+++ precache-manifest.6ff8e83217feef616a307d41951e26a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd086c07067282e478b0",
+    "url": "/IVR/static/js/runtime~main.bd086c07.js"
+  },
+  {
+    "revision": "9dd12a796e12b11bf807",
+    "url": "/IVR/static/js/main.9dd12a79.chunk.js"
+  },
+  {
+    "revision": "281bac7b2503e9ee17db",
+    "url": "/IVR/static/js/1.281bac7b.chunk.js"
+  },
+  {
+    "revision": "9dd12a796e12b11bf807",
+    "url": "/IVR/static/css/main.5852c5c1.chunk.css"
+  },
+  {
+    "revision": "281bac7b2503e9ee17db",
+    "url": "/IVR/static/css/1.f23e509d.chunk.css"
+  },
+  {
+    "revision": "7a6ee493dfa74a6d9a314b5efbb37b77",
+    "url": "/IVR/index.html"
+  }
+];
